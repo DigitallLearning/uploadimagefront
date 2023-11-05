fcc96cd6b4de7b71457204ff5f3d368e06ae5a96
digitlalearning <digitallearning1505@gmail.com>
2023-11-05 22:30:47 +0530

    new6

--- src/GetData.js
+++ src/GetData.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import Table from 'react-bootstrap/Table';
+import {Container ,Card,Row, Col, Button} from 'react-bootstrap';  
 function GetData()
 {
     const [data1,setData]=useState([])
@@ -16,26 +16,48 @@ function GetData()
    },[])
     return(
         <div>
-           <Table striped bordered hover variant="dark">
+           {/* <Table striped bordered hover variant="dark">
             <tbody>
                 <tr>
-                  
-                    <td>Name</td>
-                    <td>Image</td>
+                    <td>Product Id</td>
+                    <td>Product Name</td>
+                    <td>Product Desc</td>
+                    <td>Product Price</td>
+                    <td>Product Image</td>
                 </tr>
-                {
-                    
+                { 
                     data1.map((item,k)=>
-                      
-                        <tr key={k}>
-                        <td>{item.name}</td>  
-                       
-                        <td><img src={item.image} width="200px" height="200px"/></td>
+                        <tr>
+                        <td>{item.pid}</td> 
+                        <td>{item.pname}</td>  
+                        <td>{item.pdesc}</td>
+                        <td>{item.pprice}</td>
+                        <td><img src={item.pimage} width="200px" height="200px"/></td>
                       </tr>  
                     )
                 }
             </tbody>
-           </Table >
+           </Table > */}
+
+<Container className='p-4'>  
+  <Row>      
+ {
+ data1.map((item) => (  
+  <Card  
+    style={{width:"200px"}}  
+    className="m-2"  
+  >  
+    <Card.Img src={item.pimage} style={{height:"100px"}}/>
+    <Card.Body>  
+      <Card.Title style={{color:"red", fontSize:"15px"}}>{item.pname.substring(0, 20)}</Card.Title>
+      <Card.Title style={{color:"blue", fontSize:"15px"}}>{item.pprice} Rs</Card.Title>  
+      <Card.Text style={{color:"black", fontSize:"10px"}}>{item.pdesc.substring(0, 150)}</Card.Text> 
+      <Button variant="primary">Buy Now</Button>
+    </Card.Body>  
+  </Card>  
+))}  
+</Row>  
+</Container> 
         </div>
     )
 }
--- src/PostData.js
+++ src/PostData.js
@@ -2,8 +2,12 @@ import { useState } from "react";
 import axios from "axios";
 
 function PostData() {
-  const [image, setImage] = useState('')
-  const [name, setName] = useState('')
+  const [pid, setId] = useState('')
+  const [pname, setName] = useState('')
+  const [pdesc, setDesc] = useState('')
+  const [pprice, setPrice] = useState('')
+  const [pimage, setImage] = useState('')
+ 
 
   const handleChange = (e) => {
     console.log(e.target.files)
@@ -15,8 +19,12 @@ function PostData() {
     
     const url = "https://uploadimage-t8wr.onrender.com/"
     const formData = new FormData()
-    formData.append('file', image)
-    formData.append('name', name)
+    formData.append('pid', parseInt(pid))
+    formData.append('pname', pname)
+    formData.append('pdesc', pdesc)
+    formData.append('pprice', parseInt(pprice))
+    formData.append('pimage', pimage)
+   
     axios.post(url, formData).then(result => {
       console.log(result.data)
       alert('success')
@@ -25,7 +33,10 @@ function PostData() {
 
   return (
     <div>
-     <input type="text" placeholder="Enter Name" onChange={(e)=>setName(e.target.value)} /> <br />
+      <input type="number" placeholder="Product Id" onChange={(e)=>setId(e.target.value)} /> <br />
+     <input type="text" placeholder="Product Name" onChange={(e)=>setName(e.target.value)} /> <br />
+     <input type="text" placeholder="Product Description" onChange={(e)=>setDesc(e.target.value)} /> <br />
+     <input type="number" placeholder="Product Price" onChange={(e)=>setPrice(e.target.value)} /> <br />
      <input type="file" onChange={handleChange} /> <br />
       <button onClick={handleApi} >SUBMIT</button>
     </div>
