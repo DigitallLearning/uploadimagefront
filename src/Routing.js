c38d1687ef22560c081b67fedab34df34192e07d
digitlalearning <digitallearning1505@gmail.com>
2023-11-05 18:20:42 +0530

    new2

--- src/Routing.js
+++ src/Routing.js
@@ -22,7 +22,7 @@ function Rounting()
       <br></br> 
         <Routes>
            
-            <Route path="/" element={<Rounting/>}/>
+        
             <Route path="/post" element={<PostData/>}/>
             <Route path="/get"  element={<GetData/>}/>
         </Routes>
