56aa69bee66c8115ce3ceee8381be3a62c81c0c1
digitlalearning <digitallearning1505@gmail.com>
2023-11-05 23:36:27 +0530

    nav

---
+++ src/Nav.js
@@ -0,0 +1,24 @@
+import "./routing.css"
+import { NavLink } from 'react-router-dom'
+function Nav()
+{
+    return(
+        <nav className="navbar">
+        <div className="container">
+        <div className="logo">
+        
+        </div>
+        <div className="nav-elements">
+          <ul>
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/post">Post Data</NavLink></li>
+            <li><NavLink to="/get">View Data</NavLink></li>
+            <li><NavLink to="/about">About</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    )
+}
+export default Nav
--- src/Routing.js
+++ src/Routing.js
@@ -1,18 +1,16 @@
 import { BrowserRouter, Link, Route, Routes} from "react-router-dom"
 import GetData from "./GetData"
 import PostData from "./PostData"
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import "./routing.css"
+import Nav from "./Nav"
 function Rounting()
 {
      return(
         <div>
-        <BrowserRouter >
-         <Link to="/">Home Page</Link><br></br>
-         <Link to="/post">Post Page</Link><br></br>
-         <Link to="/get">Get Page</Link><br></br> 
+        <BrowserRouter>
+        <Nav></Nav><br></br>
         <Routes> 
+            <Route path="/" element={<GetData/>}/>
             <Route path="/post" element={<PostData/>}/>
             <Route path="/get"  element={<GetData/>}/>
         </Routes>
